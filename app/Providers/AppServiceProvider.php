09b9568d492db5086c378ed3f1a32d1717bfe73d
Tim Prima <dev@stockhub.com>
2026-01-27 20:56:20 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // PERBAIKAN: Menggunakan 'instance' agar tidak error di VS Code
+        // Logikanya: Jika folder ../public_html ditemukan, beri tahu Laravel
+        // bahwa 'path.public' yang resmi adalah folder tersebut.
+
+        if (is_dir(base_path('../public_html'))) {
+            $this->app->instance('path.public', base_path('../public_html'));
+        }
     }
 
     /**
