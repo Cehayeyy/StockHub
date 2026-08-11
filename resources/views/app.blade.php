6d0a03124b09267961b0d40b26e380cc82f86152
welres <welres@users.noreply.github.com>
2026-08-11 16:48:43 +0700

    update logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'stockhub') }}</title>
 
         <link rel="icon" type="image/png" href="{{ asset('images/stockhub-logo.png') }}">
         <link rel="preconnect" href="https://fonts.googleapis.com">
